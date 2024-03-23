8e392ba7e06de59e94502e9e731fcd71ea231ef1
Raghav-rv28 <raghavrudhra28@gmail.com>
2024-03-23 01:39:24 -0400

    fixing env variable issues

--- Windows/delete.bat
+++ Windows/delete.bat
@@ -7,8 +7,8 @@ rem Remove the decrypt executable from System32 directory
 del "%SystemRoot%\System32\decrypt.exe" /f /q
 
 rem Clear environment variables
-setx ENCRYPTION_KEY ""
-setx WATCH_DIRECTORY ""
+setx encryption_key ""
+setx watch_directory ""
 
 echo Uninstallation complete.
 echo Environment variables ENCRYPTION_KEY and WATCH_DIRECTORY have been cleared.
